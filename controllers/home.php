7d8a9267e7557e950a36e92c451449af5b8a5c0f
Davide <davide.deacetis13@gmail.com>
2022-05-17 13:10:36 +0200

    Prova per la pagina home

--- controllers/home.php
+++ controllers/home.php
@@ -1,9 +1,28 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT']."/include/template2.inc.php";
 //require "include/dbms.inc.php";
+//global $mysqli;
 
-$main = new Template($_SERVER['DOCUMENT_ROOT']."/skins/wizym/dtml/index.html");
-$main->setContent("title","Wine Boutique");
+$main = new Template($_SERVER['DOCUMENT_ROOT']."/skins/wizym/dtml/components/main.html");
+$body = new Template($_SERVER['DOCUMENT_ROOT']."/skins/wizym/dtml/home.html");
+/*$oid = $mysqli->query("");
+if(!$oid) {
+    echo "Errore nella query: ".$mysqli->error;
+    exit;
+}
+*/
+$data = array(
+    "image" => "/skins/wizym/image/homepage77.png",
+    "name" => "Vino Rosso",
+    "price" => "19.99",
+);
+
+foreach ($data as $key => $value) {
+    $body->setContent($key,$value);
+}
+
+
+$main->setContent("header", (new Template($_SERVER['DOCUMENT_ROOT']."/skins/wizym/dtml/components/header.html"))->get());
+$main->setContent("footer", (new Template($_SERVER['DOCUMENT_ROOT']."/skins/wizym/dtml/components/footer.html"))->get());
+$main->setContent("content",$body->get());
 $main->close();
-echo "prova";
-?>
