5603aee7874290007d5426821be7bb4371f59018
sant1dom <santone.domenico2k@gmail.com>
2022-07-03 16:57:30 +0200

    Differenziazione errori admin e utente

--- controllers/errors.php
+++ controllers/errors.php
@@ -3,30 +3,29 @@ require $_SERVER['DOCUMENT_ROOT'] . "/include/template.inc.php";
 //require "include/dbms.inc.php";
 //global $mysqli;
 
-//TODO: parametrizzare i messaggi di errore
-$error = "404";
-$title = "Pagina non trovata";
-$description = "La pagina che stai cercando non esiste";
+if(!str_starts_with($_SERVER['REQUEST_URI'], "/admin/")){
+    //TODO:parametrizzare i messaggi di errore
+    $error = "404";
+    $title = "Pagina non trovata";
+    $description = "La pagina che stai cercando non esiste";
 
-$main = new Template($_SERVER['DOCUMENT_ROOT']."/skins/wizym/dtml/components/main.html");
-$body = new Template($_SERVER['DOCUMENT_ROOT']."/skins/wizym/dtml/error.html");
-/*$oid = $mysqli->query("");
-if(!$oid) {
-    echo "Errore nella query: ".$mysqli->error;
-    exit;
+    $main = new Template($_SERVER['DOCUMENT_ROOT'] . "/skins/wizym/dtml/components/main.html");
+    $body = new Template($_SERVER['DOCUMENT_ROOT'] . "/skins/wizym/dtml/error.html");
+    $data = array(
+        "error" => $error,
+        "title" => $title,
+        "description" => $description,
+    );
+
+    foreach ($data as $key => $value) {
+        $body->setContent($key, $value);
+    }
+
+    $main->setContent("header", (new Template($_SERVER['DOCUMENT_ROOT'] . "/skins/wizym/dtml/components/header.html"))->get());
+    $main->setContent("footer", (new Template($_SERVER['DOCUMENT_ROOT'] . "/skins/wizym/dtml/components/footer.html"))->get());
+    $main->setContent("content", $body->get());
+    $main->close();
+} else {
+    $main = new Template($_SERVER['DOCUMENT_ROOT'] . "/skins/admin/sash/dtml/views/404.html");
+    $main->close();
 }
-*/
-$data = array(
-    "error" => $error,
-    "title" => $title,
-    "description" => $description,
-);
-
-foreach ($data as $key => $value) {
-    $body->setContent($key,$value);
-}
-
-$main->setContent("header", (new Template($_SERVER['DOCUMENT_ROOT']."/skins/wizym/dtml/components/header.html"))->get());
-$main->setContent("footer", (new Template($_SERVER['DOCUMENT_ROOT']."/skins/wizym/dtml/components/footer.html"))->get());
-$main->setContent("content",$body->get());
-$main->close();
