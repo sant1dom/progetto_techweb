a3d3f79345a2ea6d94a26ae41205b6c5b6ad18ef
Davide <davide.deacetis13@gmail.com>
2022-07-11 13:06:19 +0200

    Fix vari su viste e routes

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ error_reporting(E_ALL);
 
 $request = strtok($_SERVER["REQUEST_URI"], '?');
 const __CONTROLLERS__ = __DIR__ . '/controllers/';
-const __PUBLIC_DIRS__ = ["/", "/about", "/contacts", "/login", "/logout", "/register"];
 global $mysqli;
 
 $query = "SELECT * FROM services where '" . $request . "' like url ORDER BY LENGTH(url) DESC LIMIT 1;";
@@ -20,7 +19,7 @@ $oid = $mysqli->query($query);
 
 if ($oid->num_rows > 0) {
     $oid = $oid->fetch_assoc();
-    if (in_array($oid['url'], __PUBLIC_DIRS__)) {       //se si accede a una pagina pubblica
+    if (!(str_starts_with($oid['script'], "user") || str_starts_with($oid['script'], "admin"))) {       //se si accede a una pagina pubblica
         $controller = $oid['script'];                           //carico il controller
         $action = $oid['callback'];                             //carico la funzione da eseguire
         $controller = __CONTROLLERS__ . $controller;            //carico il file del controller
